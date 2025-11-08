a5078f34ddd0c3bdcfcc6ff83c47dd8cd91d6dc1
Bapparaj sk <bapparajsk97@gmail.com>
2025-11-08 11:59:54 +0530

    feat: integrate Tailwind CSS and NativeWind
    This commit integrates Tailwind CSS and NativeWind for styling.
    It removes the old theme system and components, and configures
    Tailwind CSS with NativeWind.  This provides a more flexible and
    powerful styling solution.

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+       fontFamily: {
+        // Poppins
+        "poppins-thin": ["PoppinsThin", "sans-serif"],
+        "poppins-light": ["PoppinsLight", "sans-serif"],
+        "poppins": ["PoppinsRegular", "sans-serif"],
+        "poppins-medium": ["PoppinsMedium", "sans-serif"],
+        "poppins-semibold": ["PoppinsSemiBold", "sans-serif"],
+        "poppins-bold": ["PoppinsBold", "sans-serif"],
+        "poppins-extrabold": ["PoppinsExtraBold", "sans-serif"],
+
+        // Inter
+        "inter-thin": ["InterThin", "sans-serif"],
+        "inter-light": ["InterLight", "sans-serif"],
+        "inter": ["InterRegular", "sans-serif"],
+        "inter-medium": ["InterMedium", "sans-serif"],
+        "inter-semibold": ["InterSemiBold", "sans-serif"],
+        "inter-bold": ["InterBold", "sans-serif"],
+        "inter-extrabold": ["InterExtraBold", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
