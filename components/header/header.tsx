86ca1a893310da1c3939d378ebd8bb4fcbf8f09c
Bapparaj sk <bapparajsk97@gmail.com>
2025-12-16 23:49:46 +0530

    feat: replace BellIcon with custom SVG for improved icon rendering in Header component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -38,10 +38,24 @@ export default function Header() {
                     {({ pressed, hovered }) => {
                         return (
                             <ButtonWrapper rounded="full" hovered={hovered} pressed={pressed} style={{ backgroundColor: colors.black }}>
-                                <BellIcon
-                                    size={24}
-                                    color={colors.white}
-                                />
+                                <Svg
+                                    width={20}
+                                    height={20}
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke={"white"}
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <Path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <Path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                    <Path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
+                                    <Path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                    <Path d="M17 10h2a2 2 0 0 1 2 2v1" />
+                                    <Path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                    <Path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
+                                </Svg>
                             </ButtonWrapper>
                         )
                     }}
